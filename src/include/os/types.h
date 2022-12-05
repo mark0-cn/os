f924424684ec59d544eb6a148a543583e7d24ac9
mark0 <59284400+mark0-cn@users.noreply.github.com>
2022-12-05 20:29:38 +0800

    增加数据类型

--- src/include/os/types.h
+++ src/include/os/types.h
@@ -1,7 +1,15 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-#define char int8
 #define _packed __attributes__((packed))
 
+typedef char int8;
+typedef short int16;
+typedef int int32;
+typedef long long int64;
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
 #endif
